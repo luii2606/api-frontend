ae21c6fcf7f9d3d057c85b1ad6c513f8581e31d6
LilJulian <juliadar267@gmail.com>
2025-05-23 11:14:20 -0500

    tablas

--- ciudades/ciudades.js
+++ ciudades/ciudades.js
@@ -1,15 +1,15 @@
-import { limpiar, validar, validarMin } from "../modulos/modulo.js";
 import { get } from "../api.js";
+import { limpiar, validar, validarMin } from "../modulos/modulo.js";
 import { crearTablaCiudades } from "./subirDatos.js";
 
-const ciudades=await get("ciudades");
+const ciudades = await get("ciudades");
+console.log(ciudades);
 
-crearTablaCiudades(["ID","Ciudad"],ciudades.data);
+crearTablaCiudades(["ID", "Ciudad"], ciudades.data);
 
+const formulario = document.querySelector("form");
+const nombre_ciudad = document.querySelector('[name="ciudad"]');
 
-const formulario=document.querySelector("form");
-const nombre_ciudad=document.querySelector('[name="ciudad"]');
-
-formulario.addEventListener('submit',(event)=>{validar(event,"ciudades")});
-nombre_ciudad.addEventListener('blur',limpiar)
-nombre_ciudad.addEventListener('blur',validarMin)
+formulario.addEventListener('submit', (event) => { validar(event, "ciudades") });
+nombre_ciudad.addEventListener('blur', limpiar)
+nombre_ciudad.addEventListener('blur', validarMin)
